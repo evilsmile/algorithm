b7ef32091904eaf97fa32ce50ef740b8957d12e9
evilsmile <freedominmind@163.com>
2017-04-09 12:22:22 +0800

    add check() in common file data.h

--- data.h
+++ data.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 
-int test_data[] = {9, 20, 30, 14, 22, 88, 443, 82, 2302, 223, 120};
+int test_data[] = {9, 50, 30, 14, 22, 88, 443, 82, 2302, 223, 120};
 
 #define TEST_DATA_SIZE sizeof(test_data)/sizeof(test_data[0])
 
@@ -23,6 +23,22 @@ void swap(int& a, int& b)
     int tmp = a;
     a = b;
     b = tmp;
+}
+
+int check(int data[], int size)
+{
+    if (data == NULL) 
+    {
+        std::cerr << "data can't be NULL!" << std::endl;
+        return -1;
+    }
+    if (size <= 0)
+    {
+        std::cerr << "size must be greater than 0!" << std::endl;
+        return -1;
+    }
+
+    return 0;
 }
 
 #endif

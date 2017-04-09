9befac4f6cbb66942ddda1a8202e27d693aacae5
evilsmile <freedominmind@163.com>
2017-04-09 12:08:03 +0800

    add insert

---
+++ insert/insert.cpp
@@ -0,0 +1,38 @@
+#include "../data.h"
+
+/*
+   插入排序
+   保证前面有序
+ */
+void insert(int data[], int size)
+{
+    if (size <= 0) {
+        std::cerr << "size must be greater than 0" << std::endl;
+        return;
+    }
+
+    for (int i = 1; i < size; ++i) 
+    {
+        int j = i;
+        int tmp = data[i];
+        while (j > 0 && data[j - 1] > tmp) 
+        {
+            // 比data[i]位置的数据大则后移
+            data[j] = data[j-1];
+            --j;
+        }
+        data[j] = tmp;
+    }
+}
+
+
+int main()
+{
+    print_data("Before sort: ", test_data, TEST_DATA_SIZE);
+
+    insert(test_data, TEST_DATA_SIZE);
+
+    print_data("After sort: ", test_data, TEST_DATA_SIZE);
+
+    return 0;
+}
